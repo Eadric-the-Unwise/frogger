79f060fe665a78881429c633caa9fd481cd40dc9
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2023-02-25 08:50:20 -0800

    all cars scrolling at correct speeds

--- src/main.c
+++ src/main.c
@@ -3,12 +3,16 @@
 #include <stdio.h>
 
 #include "scene.h"
-
+//------------GOALS-------------//
+// SCREEN SCROLL + LYC_REG EDIT
+// CGB PALLETES
+// SOUND
 GameCharacter PLAYER;
 UINT8 joy, last_joy;
 UBYTE is_moving;
 INT8 move_x, move_y;
-UINT8 scroll1, scroll2, scroll3, scroll4;
+UINT8 scroll1, scroll2, scroll3, scroll4, scroll5, scroll6, scroll7, scroll8;
+UINT8 scx_counter;
 
 void move_frog() {
     if (move_x != 0) {
@@ -23,18 +27,6 @@ void move_frog() {
 }
 
 void parallaxScroll() {
-    // if (LYC_REG == 0x00) {
-    //     move_bkg(scroll1, 0);
-    //     LYC_REG = 0x30;
-    // }
-    // if (LYC_REG == 0x30) {
-    //     move_bkg(scroll2, 0);
-    //     LYC_REG = 0x64;
-    // }
-    // if (LYC_REG == 0x64) {
-    //     move_bkg(scroll3, 0);
-    //     LYC_REG = 0x00;
-    // }
     switch (LYC_REG) {
         case 0x00:
             move_bkg(scroll1, 0);
@@ -46,10 +38,26 @@ void parallaxScroll() {
             break;
         case 0x48:
             move_bkg(scroll3, 0);
+            LYC_REG = 0x50;
+            break;
+        case 0x50:
+            move_bkg(scroll4, 0);
+            LYC_REG = 0x58;
+            break;
+        case 0x58:
+            move_bkg(scroll5, 0);
+            LYC_REG = 0x60;
+            break;
+        case 0x60:
+            move_bkg(scroll6, 0);
+            LYC_REG = 0x68;
+            break;
+        case 0x68:
+            move_bkg(scroll7, 0);
             LYC_REG = 0x6F;
             break;
         case 0x6F:
-            move_bkg(scroll4, 0);
+            move_bkg(scroll8, 0);
             LYC_REG = 0x00;
             break;
     }
@@ -58,8 +66,6 @@ void parallaxScroll() {
 void main() {
     STAT_REG = 0x45;  // enable LYC=LY interrupt so that we can set a specific line it will fire at
     LYC_REG = 0x00;
-
-    scroll1 = scroll2 = scroll3 = scroll4 = 0;
 
     CRITICAL {
         add_LCD(parallaxScroll);
@@ -74,12 +80,12 @@ void main() {
     DISPLAY_ON;
 
     set_sprite_data(0, 4, frogger_tiles);
-    set_bkg_data(0, 15, BKG_TILES);
+    set_bkg_data(0, 17, BKG_TILES);
     set_bkg_tiles(0, 0, 32, 32, BKG_MAP);
 
     PLAYER.x = 56;
     PLAYER.y = 108;
-    UINT8 scx_counter = 2;  // REMOVE THIS?
+
     move_metasprite(
         frogger_metasprites[0], 0, 0, PLAYER.x, PLAYER.y);
 
@@ -127,15 +133,19 @@ void main() {
             is_moving = FALSE;
 
         // --------------------MOVE FROG -------------------------------//
-        scroll1 += 1;
-        scroll2 -= 0;
-        scroll3 += 1;
-        // if (scx_counter == 1) {
-        //     SCX_REG++;
-        // }
-        // if (scx_counter == 0)
-        //     scx_counter = 2;
-        // scx_counter--;
+
+        if (scx_counter % 4 == 0) {  // += moves the 'camera' to the right, resulting in objects on screen moving left
+            scroll1 -= 1;            // WATER
+            scroll5 += 1;            // CAR3
+            scroll6 -= 1;            // CAR4
+            scroll7 += 1;            // CAR5
+        }
+        if (scx_counter % 3 == 0) {
+            scroll3 += 1;  // BUS
+        }
+        scroll4 -= 1;  // PINK CAR
+
+        scx_counter++;
 
         if (joy & J_SELECT) {
             PLAYER.x = 56;

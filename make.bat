07c3c099bf5f5940479fd5c367b2e69b653b281b
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2023-05-02 16:03:00 -0700

    removed hUGEtracker

--- make.bat
+++ make.bat
@@ -9,7 +9,7 @@ call .\clean.bat
 @REM -Wl-j .noi bgb debugging -Wm-yS
 @set GBDK=..\..\..\gbdk
 %GBDK%\bin\lcc.exe -autobank -Wf--debug -Wl-y -Wb-ext=.rel -Wb-v -Wl-yt0x1B -Wl-yoA -Wl-ya4 -o frogger-clone.gb^
- src\*.c sound\*.c res\tiles\*.c sound\hUGEDriver.obj.o
+ src\*.c res\tiles\*.c
 
  
 
